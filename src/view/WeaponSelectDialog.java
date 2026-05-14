077d8363920216dd54a3b2dc278a95e9a242fb62
Albin <albint252@gmail.com>
2026-05-15 01:19:02 +0200

    Levande meny

--- src/view/WeaponSelectDialog.java
+++ src/view/WeaponSelectDialog.java
@@ -67,6 +67,8 @@ public class WeaponSelectDialog {
         double[] sSpeed = new double[STAR_COUNT];
         double[] sPhase = new double[STAR_COUNT];
         double[] sBaseAlpha = new double[STAR_COUNT];
+        double[] sTwinklePhase = new double[STAR_COUNT];
+        double[] sTwinkleSpeed = new double[STAR_COUNT];
 
         for (int i = 0; i < STAR_COUNT; i++) {
             sx[i] = rand.nextDouble() * width;
@@ -75,7 +77,18 @@ public class WeaponSelectDialog {
             sSpeed[i] = 0.2 + rand.nextDouble() * 0.8;
             sPhase[i] = rand.nextDouble() * Math.PI * 2;
             sBaseAlpha[i] = 0.3 + rand.nextDouble() * 0.7;
+            sTwinklePhase[i] = rand.nextDouble() * Math.PI * 2;
+            sTwinkleSpeed[i] = 0.3 + rand.nextDouble() * 0.7;
         }
+
+        double[] ssActive   = {0};
+        double[] ssX        = {0};
+        double[] ssY        = {0};
+        double[] ssVx       = {0};
+        double[] ssVy       = {0};
+        double[] ssLife     = {0};
+        double[] ssMaxLife  = {0};
+        double[] ssCooldown = {rand.nextDouble() * 7 + 8};
 
         starAnimation = new AnimationTimer() {
             private long lastNano = 0;
@@ -99,8 +112,69 @@ public class WeaponSelectDialog {
                     double twinkle = 0.5 + 0.5 * Math.sin(timeSec * (1.5 + sSpeed[i]) + sPhase[i]);
                     double alpha = sBaseAlpha[i] * (0.3 + 0.7 * twinkle);
 
-                    gc.setFill(Color.gray(1.0, alpha));
+                    double bluePulse = Math.sin(timeSec * sTwinkleSpeed[i] + sTwinklePhase[i]);
+                    if (bluePulse > 0.85) {
+                        double blueIntensity = (bluePulse - 0.85) / 0.15;
+                        gc.setFill(Color.color(0.7 + 0.3 * (1 - blueIntensity), 0.85 + 0.15 * (1 - blueIntensity), 1.0, Math.min(1.0, alpha + 0.3 * blueIntensity)));
+                    } else {
+                        gc.setFill(Color.gray(1.0, alpha));
+                    }
                     gc.fillOval(sx[i], sy[i], sSize[i], sSize[i]);
+                }
+
+                // --- Shooting star ---
+                if (ssActive[0] == 0) {
+                    ssCooldown[0] -= dt;
+                    if (ssCooldown[0] <= 0) {
+                        ssActive[0] = 1;
+                        ssMaxLife[0] = 0.5 + rand.nextDouble() * 0.5;
+                        ssLife[0] = ssMaxLife[0];
+                        if (rand.nextBoolean()) {
+                            ssX[0] = rand.nextDouble() * width * 0.7;
+                            ssY[0] = rand.nextDouble() * height * 0.3;
+                        } else {
+                            ssX[0] = rand.nextDouble() * width * 0.3;
+                            ssY[0] = rand.nextDouble() * height * 0.5;
+                        }
+                        double speed = 300 + rand.nextDouble() * 200;
+                        double angle = Math.toRadians(25 + rand.nextDouble() * 30);
+                        ssVx[0] = speed * Math.cos(angle);
+                        ssVy[0] = speed * Math.sin(angle);
+                    }
+                }
+                if (ssActive[0] == 1) {
+                    ssX[0] += ssVx[0] * dt;
+                    ssY[0] += ssVy[0] * dt;
+                    ssLife[0] -= dt;
+                    if (ssLife[0] <= 0) {
+                        ssActive[0] = 0;
+                        ssCooldown[0] = 8 + rand.nextDouble() * 7;
+                    } else {
+                        int tailSegments = 20;
+                        double progress = 1.0 - (ssLife[0] / ssMaxLife[0]);
+                        double tailLength = 80 + 60 * (1.0 - progress);
+                        double vLen = Math.sqrt(ssVx[0] * ssVx[0] + ssVy[0] * ssVy[0]);
+                        double nx = ssVx[0] / vLen;
+                        double ny = ssVy[0] / vLen;
+                        double lifeFade = ssLife[0] < ssMaxLife[0] * 0.3
+                                ? ssLife[0] / (ssMaxLife[0] * 0.3) : 1.0;
+
+                        gc.save();
+                        for (int t = 0; t < tailSegments; t++) {
+                            double frac = t / (double) tailSegments;
+                            double tx = ssX[0] - nx * tailLength * frac;
+                            double ty = ssY[0] - ny * tailLength * frac;
+                            double a = (1.0 - frac) * lifeFade;
+                            double size = 3.0 * (1.0 - frac * 0.7);
+                            double r = 1.0 - 0.3 * frac;
+                            double g = 1.0 - 0.15 * frac;
+                            double b = Math.min(1.0, 0.9 + 0.1 * frac);
+                            gc.setGlobalAlpha(Math.max(0, Math.min(1.0, a)));
+                            gc.setFill(Color.color(r, g, b));
+                            gc.fillOval(tx - size / 2, ty - size / 2, size, size);
+                        }
+                        gc.restore();
+                    }
                 }
             }
         };
